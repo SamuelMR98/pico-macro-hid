2a64b8a7dde8aa4db14757fad497d0ff2c2e9e00
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2026-03-25 19:50:43 -0600

    added config/pins

---
+++ firmware/config/pins.h
@@ -0,0 +1,16 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Pin definitions for the board
+
+#define BUTTON_COUNT 4
+
+// Button pins
+#define BUTTON_1_PIN 2
+#define BUTTON_2_PIN 3
+#define BUTTON_3_PIN 4
+#define BUTTON_4_PIN 5
+
+#define DEBOUNCE_MS 25 // Debounce time for buttons in milliseconds
+
+#endif // PINS_H
